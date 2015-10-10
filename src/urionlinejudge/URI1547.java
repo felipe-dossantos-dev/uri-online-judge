e155ed864a891520f4cb8b53368bf02caf4baf10
felipe <felipe@felipe-pc>
2015-10-10 16:05:17 -0300

    Primeiro commit com um exercício do URI que fiz hoje

---
+++ src/urionlinejudge/URI1547.java
@@ -0,0 +1,40 @@
+package urionlinejudge;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
+
+public class URI1547 {
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(
+                System.in));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
+                System.out));
+        int qtdTestes = Integer.parseInt(entrada.readLine());
+        while (qtdTestes != 0) {
+            String linha = entrada.readLine();
+            String vet[] = linha.split(" ");
+            int qtdAlunos = Integer.parseInt(vet[0]);
+            int secreto = Integer.parseInt(vet[1]);
+            int distMenor = Integer.MAX_VALUE;
+            int posMenor = -1;
+            linha = entrada.readLine();
+            vet = linha.split(" ");
+            for (int i = 0; i < qtdAlunos; i++) {
+                int atual = Integer.parseInt(vet[i]);
+                int a = Math.abs(secreto - atual);
+                if (a < distMenor) {
+                    distMenor = a;
+                    posMenor = i + 1;
+                }
+            }
+            saida.write(posMenor+"\n");
+            qtdTestes--;
+        }
+        saida.flush();
+    }
+}
---
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -0,0 +1,19 @@
+package urionlinejudge;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
+
+public class UriOnlineJudge {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(
+                System.in));
+        BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
+                System.out));
+        
+    }
+}
