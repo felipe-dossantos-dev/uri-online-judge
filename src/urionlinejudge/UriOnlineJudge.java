f43dd43f4bd4e7931a77e5a7ca9667f248379760
felipe.santos <santos.flip@gmail.com>
2015-11-10 11:44:26 -0200

    +1

--- src/urionlinejudge/UriOnlineJudge.java
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class UriOnlineJudge {
@@ -15,54 +16,54 @@ public class UriOnlineJudge {
                 System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
-        String linha = entrada.readLine();
-        while (linha != null) {
-            String vet[] = linha.split(" ");
-            int cont = vet.length - 1;
-            int res = 0;
-            boolean flag = true;
-            while (cont > 0) {
-                try {
-                    int b = Integer.parseInt(vet[cont--]);
-                    int a = Integer.parseInt(vet[cont--]);
-                    if (a > 9 || b > 9) {
-                        throw new NumberFormatException();
-                    }
-                    String oper = vet[cont];
-                    switch (oper) {
-                        case "/":
-                            if (a == 0)
-                                throw new ArithmeticException();
-                            else 
-                                res = b / a;
-                            break;
-                        case "*":
-                            res = b * a;
-                            break;
-                        case "-":
-                            res = b - a;
-                            break;
-                        case "+":
-                            res = b + a;
-                            break;
-                    }
-                    vet[cont] = Integer.toString(res);
-                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    saida.write("Invalid expression.\n");
-                    flag = false;
-                    break;
-                } catch (ArithmeticException e) {
-                    saida.write("Division by zero.\n");
-                    flag = false;
-                    break;
+        int qtd = Integer.parseInt(entrada.readLine());
+        while (qtd != 0) {
+            long[] somas = new long[qtd];
+            int[] valores = new int[qtd];
+            String nomes[] = new String[qtd];
+
+            for (int i = 0; i < qtd; i++) {
+                String linha = entrada.readLine();
+                nomes[i] = linha;
+                int valor = 0;
+                char letras[] = linha.toCharArray();
+                for (char letra : letras) {
+                    valor += letra;
+                }
+                valores[i] = valor;
+                if (i == 0) {
+                    somas[i] = valor;
+                } else {
+                    somas[i] = somas[i - 1] + valor;
                 }
             }
-            if (flag) {
-                saida.write("The answer is " + res + ".\n");
+            //buscas
+            long busca = somas[qtd - 1] / 2 + 1;
+            int busca2 = Arrays.binarySearch(somas, busca);
+            if (busca2 < 0)
+                busca2 += qtd;
+            if (busca > somas[busca2])
+                busca2++;
+
+            long timeA = 0;
+            int cont = 1;
+            for (int i = busca2; i >=0 ; i--) {
+                timeA += valores[i] * cont;
+                cont++;
             }
-            linha = entrada.readLine();
-            saida.flush();
+            long timeB = 0;
+            cont = 1;
+            for (int i = busca2 + 1; i < qtd; i++) {
+                timeB += valores[i] * cont;
+                cont++;
+            }
+            if (timeB != timeA) {
+                saida.write("Impossibilidade de empate.\n");
+            } else {
+                saida.write(nomes[busca2] + "\n");
+            }
+            qtd = Integer.parseInt(entrada.readLine());
         }
-        
+        saida.flush();
     }
 }
