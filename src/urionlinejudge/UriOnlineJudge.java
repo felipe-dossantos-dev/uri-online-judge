43e01c918a01afd7fa5b36822164d47c21c0d6c1
felipe.santos <santos.flip@gmail.com>
2015-11-30 16:01:43 -0200

    +1

--- src/urionlinejudge/UriOnlineJudge.java
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -5,10 +5,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class UriOnlineJudge {
 
@@ -18,25 +17,58 @@ public class UriOnlineJudge {
                 System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
-        List<List<Integer>> listas = new ArrayList<>(5);
-        for (int i = 0; i < 5; i++) {
+        int qtdCasos = Integer.parseInt(entrada.readLine());
+        Map<String, String[]> mapa = gerarMapa();
+        while (qtdCasos != 0) {
             String linha = entrada.readLine();
             String vet[] = linha.split(" ");
-            int qtd = Integer.parseInt(vet[0]);
-            List<Integer> listaAtual = new ArrayList<>();
-            for (int j = 0; j < qtd; i++) {
-                listaAtual.add(new Integer(vet[j + 1]));
+            if (vet[0].equals(vet[1])) {
+                saida.write("empate\n");
+            } else {
+                boolean rajeshGanhou = false;
+                boolean sheldonGanhou = false;
+                String rajeshGanha[] = mapa.get(vet[0]);
+                for (String ganha : rajeshGanha) {
+                    if (vet[1].equals(ganha) && !rajeshGanhou) {
+                        rajeshGanhou = true;
+                    }
+                }
+                if (!rajeshGanhou) {
+                    String sheldonGanha[] = mapa.get(vet[1]);
+                    for (String ganha : sheldonGanha) {
+                        if (vet[0].equals(ganha) && !sheldonGanhou) {
+                            sheldonGanhou = true;
+                        }
+                    }
+                }
+                if (rajeshGanhou == sheldonGanhou) {
+                    saida.write("empate\n");
+                }
+                if (rajeshGanhou) {
+                    saida.write("rajesh\n");
+                }
+                if (sheldonGanhou) {
+                    saida.write("sheldon\n");
+                }
             }
-            Collections.sort(listaAtual);
-            listas.add(listaAtual);
+            qtdCasos--;
         }
-        String linha = entrada.readLine();
-        int qtdConj = Integer.parseInt(linha);
-        int pos = -1;
-        int menorDistancia = 100000;
-        while (qtdConj != 0) {
-            qtdConj--;
-        }
+
         saida.flush();
+    }
+
+    public static Map<String, String[]> gerarMapa() {
+        Map<String, String[]> mapa = new HashMap<>();
+        String vet[] = {"papel", "lagarto"};
+        mapa.put("tesoura", vet);
+        String vet2[] = {"pedra", "spock"};
+        mapa.put("papel", vet2);
+        String vet3[] = {"lagarto", "tesoura"};
+        mapa.put("pedra", vet3);
+        String vet4[] = {"spock", "papel"};
+        mapa.put("lagarto", vet4);
+        String vet5[] = {"pedra", "tesoura"};
+        mapa.put("spock", vet5);
+        return mapa;
     }
 }
