6979fe4ae48ce1f2c973b7821ca9ed66e784e4eb
felipe.santos <santos.flip@gmail.com>
2015-12-01 15:37:45 -0200

    +1

--- src/urionlinejudge/UriOnlineJudge.java
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -5,6 +5,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 public class UriOnlineJudge {
@@ -15,26 +18,59 @@ public class UriOnlineJudge {
                 System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
+        
         String linha = entrada.readLine();
-        while (linha != null) {
-            int contAliteracao = 0;
-            boolean jaContou = false;
-            String vet[] = linha.split(" ");
-            String primeiraLetraAnterior = "*";
-            for (String palavra : vet) {
-                if (palavra.startsWith(primeiraLetraAnterior)) {
-                    if (!jaContou) {
-                        contAliteracao++;
-                    }
-                    jaContou = true;
-                } else {
-                    primeiraLetraAnterior = palavra.substring(0, 1).toLowerCase();
-                    jaContou = false;
-                }
-            }
-            saida.write(contAliteracao + "\n");
-            linha = entrada.readLine();
+        String vet[] = linha.split(" ");
+        List<Integer> listaPortugues = new ArrayList<>();
+        for (int i = 1; i < vet.length; i++) {
+            listaPortugues.add(new Integer(vet[i]));
         }
+        
+        linha = entrada.readLine();
+        vet = linha.split(" ");
+        List<Integer> listaMatematica = new ArrayList<>();
+        for (int i = 1; i < vet.length; i++) {
+            listaMatematica.add(new Integer(vet[i]));
+        }
+        
+        linha = entrada.readLine();
+        vet = linha.split(" ");
+        List<Integer> listaFisica = new ArrayList<>();
+        for (int i = 1; i < vet.length; i++) {
+            listaFisica.add(new Integer(vet[i]));
+        }
+        
+        linha = entrada.readLine();
+        vet = linha.split(" ");
+        List<Integer> listaQuimica = new ArrayList<>();
+        for (int i = 1; i < vet.length; i++) {
+            listaQuimica.add(new Integer(vet[i]));
+        }
+        
+        linha = entrada.readLine();
+        vet = linha.split(" ");
+        List<Integer> listaBiologia = new ArrayList<>();
+        for (int i = 1; i < vet.length; i++) {
+            listaBiologia.add(new Integer(vet[i]));
+        }
+        
+        listaBiologia.sort(Collections.reverseOrder());
+        listaFisica.sort(Collections.reverseOrder());
+        listaMatematica.sort(Collections.reverseOrder());
+        listaPortugues.sort(Collections.reverseOrder());
+        listaQuimica.sort(Collections.reverseOrder());
+
+        linha = entrada.readLine();
+        int iteracoes = Integer.parseInt(linha);
+        int soma = 0;
+        int menorDif = 999999;
+        
+        while(iteracoes != 0) {
+            
+            iteracoes--;
+        }
+        
+        
         saida.flush();
     }
 }
