b6146e1ff429aa48a19297340217ebad90d854a9
fgsantos89 <santos.flip@gmail.com>
2015-12-04 00:25:07 -0200

    +1

--- src/urionlinejudge/UriOnlineJudge.java
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -5,29 +5,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
 import java.util.Locale;
-<<<<<<< HEAD
-import java.util.Stack;
-=======
-import java.util.Set;
->>>>>>> 4a9c0fc2d074f375ab55e6fd6ee50c29b1b0dc73
 
 public class UriOnlineJudge {
 
-    public static String resposta;
-
-    public static void main(String[] args) throws Exception, IOException {
+ public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
         BufferedReader entrada = new BufferedReader(new InputStreamReader(
                 System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
+        //1272
         String linha = entrada.readLine();
-<<<<<<< HEAD
-=======
-        
->>>>>>> 4a9c0fc2d074f375ab55e6fd6ee50c29b1b0dc73
+        int qtdTeste = Integer.parseInt(linha);
+        for (int i = 0; i < qtdTeste; i++) {
+            linha = entrada.readLine();
+            String vet[] = linha.split("\\s+");
+            String ret = "";
+            for (int j = 0; j < vet.length; j++) {
+                if (vet[j].length() > 0) {
+                    ret += vet[j].substring(0, 1);
+                }
+            }
+            saida.write(ret);
+            saida.newLine();
+        }
         saida.flush();
     }
 }
