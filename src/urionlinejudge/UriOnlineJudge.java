3ba0dd786ddf58228591180c2a338257e71b3019
felipe.santos <santos.flip@gmail.com>
2015-12-07 15:36:46 -0200

    +1

--- src/urionlinejudge/UriOnlineJudge.java
+++ src/urionlinejudge/UriOnlineJudge.java
@@ -5,33 +5,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class UriOnlineJudge {
-    
+
     public static Set<String> conjunto;
 
     public static void main(String[] args) throws IOException {
-        //1556
         Locale.setDefault(Locale.US);
         BufferedReader entrada = new BufferedReader(new InputStreamReader(
                 System.in));
         BufferedWriter saida = new BufferedWriter(new OutputStreamWriter(
                 System.out));
         String linha = entrada.readLine();
-        conjunto = new HashSet<>();
+        conjunto = new TreeSet<>();
         while (linha != null) {
-            subs(linha.toCharArray(), 0, 0, linha.length(), linha.length());
-            List<String> listaOrdenada = new ArrayList<>(conjunto);
-            Collections.sort(listaOrdenada);
-            for (String ord : listaOrdenada) {
+            subs(linha, "", 0);
+            for (String ord : conjunto) {
                 saida.write(ord);
                 saida.newLine();
             }
@@ -41,10 +33,15 @@ public class UriOnlineJudge {
         }
         saida.flush();
     }
-    
-    public static void subs(char[] letras, int i1, int f1, int i2, int f2) {
-        if (i1 != f1) {
-            String a = new String(letras, i1, f1 - i1);
+
+    public static void subs(String palavra, String atual, int posAtual) {
+        if (atual.length() > 0) {
+            conjunto.add(atual);
+        }
+        if (atual.length() < palavra.length()) {
+            for (int i = posAtual; i < palavra.length(); i++) {
+                subs(palavra, atual + palavra.charAt(i), i + 1);
+            }
         }
     }
 }
